4a500a96bf695f2cc1f07b6430f2aea673fcec42
Vishwasp123 <vishawasparashar111@gmail.com>
2024-05-10 17:23:23 +0530

    Add image_video_in_api

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,60 @@
+class UsersController < ApplicationController
+	skip_before_action :verify_authenticity_token
+	before_action :set_user, only: %i[show update destroy]
+
+	def index
+		@users = User.all 
+		render json: @users 
+	end
+	def create
+		byebug
+		@user = User.create!(user_params)
+		render json: @user
+	end
+
+	def show
+		render json: @user.as_json(include: :images).merge(
+			images: @user.images.map { |image| url_for(image) }
+			)
+	end
+
+
+	def update
+		@user = User.update!(user_params)
+		render json: @user
+	end
+
+	
+
+	def destroy
+		if @user.destroy
+			render json: { messages: "User destroyed successfully", user: @user }
+		else
+			render json: { messages: "User could not be destroyed" }, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def set_user
+		if @user = User.find_by_id(params[:id])	
+		else
+			byebug
+			 render json: {messages: " Id Not present "}
+    end
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :age, :address, images: [])
+	end
+
+	# def attach_images_to_user
+	# 	images = params[:user][:images]
+	# 	return unless images
+
+	# 	images.each do |image|
+	# 		@user.images.attach(image)
+	# 	end
+	# end
+
+end
